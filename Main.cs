717a6951ef2fa30b4d373b190a2901c00e539430
brian-sw <brian.warren@shiftwise.com>
2018-02-27 13:25:59 -0800

    makin proguess

--- Main.cs
+++ Main.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using Octokit;
+using System.Threading.Tasks;
 
 namespace cloneit
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main() => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            var owner = "egee-irl";
+            var reponame = "egeeio";
+            var branchName = "master";
+            var path = @"/tmp/test.zip";
+
+            var client = new GitHubClient(new ProductHeaderValue(owner));
+            var repo = await client.Repository.Get(owner, reponame);
+            var downloadUrl = repo.SvnUrl + "/archive/" + branchName + ".zip";
+
+            Console.WriteLine("The download url is: " + downloadUrl);
+
+            using (var http = new System.Net.Http.HttpClient())
+            {
+                var contents = http.GetByteArrayAsync(downloadUrl).Result;
+                System.IO.File.WriteAllBytes(path, contents);
+            }
         }
     }
 }
