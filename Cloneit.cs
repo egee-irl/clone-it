9bda8413bf58768b730d313de9ee7d0df4811e45
egee-irl <brian@egee.io>
2018-02-28 07:17:22 -0800

    cut through the static, moved things out

---
+++ Cloneit.cs
@@ -0,0 +1,67 @@
+using System;
+using Octokit;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Cloneit
+{
+    public class Rekal
+    {
+        GitHubClient _client;
+        String _username;
+        String _owner;        
+        String _token;
+        String _path;
+        public Rekal(JObject json)
+        {
+            _token = Environment.GetEnvironmentVariable("GIT_TOKEN");
+            _username = json.SelectToken("username").ToString();
+            _owner = json.SelectToken("owner").ToString();
+            _path = json.SelectToken("path").ToString();
+
+            _client = new GitHubClient(new ProductHeaderValue(_username));
+            _client.Credentials = new Credentials(_token);
+        }
+
+        public async Task CloneRepos(string repoName, string branchName)
+        {
+            if (_path == "")
+                _path = "/tmp/";
+            var path = @_path + repoName + ".zip";
+            var repo = await _client.Repository.Get(_owner, repoName);
+            var downloadUrl = repo.SvnUrl + "/archive/" + branchName + ".zip";
+            using (var http = new HttpClient())
+            {
+                CredentialHelper(http);
+                var contents = http.GetByteArrayAsync(downloadUrl).Result;
+                try
+                {
+                    System.IO.File.WriteAllBytes(path, contents);
+                }
+                catch (DirectoryNotFoundException e)
+                {
+                    Console.WriteLine(e.Message + " - Directory must exist first!");
+                }                
+            }
+            try
+            {
+                ZipFile.ExtractToDirectory(path, _path);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("WARNING: " + e.Message);
+            }
+        }
+        private void CredentialHelper(HttpClient http)
+        {
+            var credentials = string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0}:", _token);
+            credentials = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(credentials));
+            http.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", credentials);
+        }  
+    }
+}
--- Main.cs
+++ Main.cs
@@ -8,63 +8,25 @@ using System.IO.Compression;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 
-namespace cloneit
+namespace Cloneit
 {
     class Program
     {
-        static GitHubClient _client;
-        static String _username;
-        static String _owner;        
-        static String _token;
-
         private static void Main() => MainAsync().GetAwaiter().GetResult();
 
         private static async Task MainAsync()
         {
             var json = JObject.Parse(File.ReadAllText(@"appsettings.json"));
-            _token = Environment.GetEnvironmentVariable("GIT_TOKEN");
-            _username = json.SelectToken("username").ToString();
-            _owner = json.SelectToken("owner").ToString();
-            _client = new GitHubClient(new ProductHeaderValue(_username));
-            
-            _client.Credentials = new Credentials(_token);
-
             var repositories = json.SelectToken("repositories");
+
+            var rekal = new Rekal(json);
             
             foreach (dynamic repo in repositories)
             {
-                await CloneRepos(repo.name.ToString(), repo.branch.ToString());
+                await rekal.CloneRepos(repo.name.ToString(), repo.branch.ToString());
             }
 
             Console.WriteLine("All done. Thank you for choosing Rekal. 👍");
-        }
-
-        private static void CredentialHelper(HttpClient http)
-        {
-            var credentials = string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0}:", _token);
-            credentials = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(credentials));
-            http.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", credentials);
-        }
-
-        private static async Task CloneRepos(string repoName, string branchName)
-        {
-            var path = @"/tmp/" + repoName + ".zip";
-            var repo = await _client.Repository.Get(_owner, repoName);
-            var downloadUrl = repo.SvnUrl + "/archive/" + branchName + ".zip";
-            using (var http = new HttpClient())
-            {
-                CredentialHelper(http);
-                var contents = http.GetByteArrayAsync(downloadUrl).Result;
-                System.IO.File.WriteAllBytes(path, contents);
-            }
-            try
-            {
-                ZipFile.ExtractToDirectory(path, "/tmp/");
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine("WARNING: " + e.Message);
-            }
         }
     }
 }
